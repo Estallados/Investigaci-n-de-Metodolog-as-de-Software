68f035d9ef4fb0cb3b975a0a7f512f9ef8a26179
Sealpaca-16 <sergiopastran16@gmail.com>
2026-09-13 01:45:35 -0500

    login

--- MetodologiaFront/src/app/metodologias/kanban/kanban.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-kanban',
-  styles: ``,
-  templateUrl: './kanban.html',
-})
-export class Kanban {}
--- MetodologiaFront/src/app/metodologias/prototipos/prototipos.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-prototipos',
-  styles: ``,
-  templateUrl: './prototipos.html',
-})
-export class Prototipos {}
