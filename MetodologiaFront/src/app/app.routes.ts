940c6fe1f6565af9e547dcb57efc4382e07b3a34
Sealpaca-16 <sergiopastran16@gmail.com>
2026-09-12 22:37:47 -0500

    wd

--- MetodologiaFront/src/app/app.routes.ts
+++ MetodologiaFront/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },{
+    path: 'Login',
+    component: Login
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
