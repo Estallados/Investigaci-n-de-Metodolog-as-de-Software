d587dd64a52be61baaad11692bb0e094f6c1a2e6
Luis Camilo Vivas Cruz <147774400+Vivas11@users.noreply.github.com>
2026-09-13 02:43:31 -0500

    feat: DashBoard no funcional

--- MetodologiaFront/src/app/panel/panel.ts
+++ MetodologiaFront/src/app/panel/panel.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   selector: 'app-panel',
   templateUrl: './panel.html',
 })
 export class Panel {
+  mostrarModal = false;
 
+  nombreProyecto = '';
+  descripcionProyecto = '';
+
+  abrirModal() {
+    this.mostrarModal = true;
+  }
+
+  cerrarModal() {
+    this.mostrarModal = false;
+    this.nombreProyecto = '';
+    this.descripcionProyecto = '';
+  }
+
+  crearProyecto() {
+
+    console.log('Nombre:', this.nombreProyecto);
+    console.log('Descripción:', this.descripcionProyecto);
+
+    // Service
+
+    this.cerrarModal();
+  }
 }
